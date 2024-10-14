66f6fc96dc5fa7357adb56e4c0012530b3ac29c8
Loukas Kotas <loukas.kotas@gmail.com>
2024-10-14 22:26:03 +0300

    add icon on the active collection

--- src/commands/set-active-collection.ts
+++ src/commands/set-active-collection.ts
@@ -27,6 +27,31 @@ export const setActiveCollectionHelper = async (
   const collections = globalState.context?.globalState.get(identifier) as BreakpointCollection[];
   const requestedCollection: BreakpointCollection | undefined = collections.find((col) => col.name === selectedCollectionName.label);
 
+  const selectedCollectionTreeItem: CollectionTreeItem | undefined = globalState.collectionProvider?.findTreeItem(selectedCollectionName.label);
+  const selectedTreeItem = globalState.collectionProvider?.getTreeItem(selectedCollectionTreeItem!);
+
+  updateIconForActiveCollection(globalState, selectedTreeItem);
+  
+  setActiveCollectionTreeItem(requestedCollection, globalState, selectedCollectionName);
+};
+
+/**
+ * The function `setActiveCollectionTreeItem` sets the active breakpoint collection based on the
+ * requested collection and displays a message accordingly.
+ * @param {BreakpointCollection | undefined} requestedCollection - The `requestedCollection` parameter
+ * is of type `BreakpointCollection | undefined`, which means it can either be a `BreakpointCollection`
+ * object or `undefined`. The function `setActiveCollectionTreeItem` uses this parameter to set the
+ * active collection based on the user's selection.
+ * @param {GlobalState} globalState - The `globalState` parameter in the `setActiveCollectionTreeItem`
+ * function is likely an object that stores global state information for your application or extension.
+ * It may contain properties and values that need to be accessed or updated throughout the application.
+ * This parameter is used to keep track of the active collection and
+ * @param selectedCollectionName - The `selectedCollectionName` parameter in the
+ * `setActiveCollectionTreeItem` function represents the label of the tree item that was selected by
+ * the user. It is used to display messages to the user indicating the status of the operation, such as
+ * showing an information message when the active collection is set or
+ */
+function setActiveCollectionTreeItem(requestedCollection: BreakpointCollection | undefined, globalState: GlobalState, selectedCollectionName: vscode.TreeItemLabel) {
   if (requestedCollection) {
     const workspace_path = vscode.workspace.workspaceFolders![0].uri.path;
     globalState.activeCollection = convertToSourceCollection(requestedCollection, workspace_path);
@@ -40,4 +65,28 @@ export const setActiveCollectionHelper = async (
       `Collection "${selectedCollectionName.label}" not found.`
     );
   }
-};
+}
+
+/**
+ * The function `updateIconForActiveCollection` updates the icon for the selected collection in a tree
+ * view while removing icons from other collection tree items.
+ * @param {GlobalState} globalState - The `globalState` parameter is an object that holds the state of
+ * the extension globally. It likely contains information about the current state of the extension,
+ * such as which collection is active or selected.
+ * @param {vscode.TreeItem | undefined} selectedTreeItem - The `selectedTreeItem` parameter is the tree
+ * item that is currently selected in the collection tree view.
+ */
+function updateIconForActiveCollection(globalState: GlobalState, selectedTreeItem: vscode.TreeItem | undefined) {
+  // remove icons from all the collection tree items in the collection provider 
+  globalState.collectionProvider?.getChildren().forEach(collectionTreeItem => collectionTreeItem.iconPath = undefined);
+
+  // update the icon on the selected collection on the collection provider
+  try {
+    if (selectedTreeItem) {
+      selectedTreeItem.iconPath = new vscode.ThemeIcon('check', new vscode.ThemeColor('charts.green'));
+      globalState.collectionProvider?.refresh();
+    }
+  } catch (e) {
+    vscode.window.showErrorMessage('Error: on selecting the tree item');
+  }
+}
