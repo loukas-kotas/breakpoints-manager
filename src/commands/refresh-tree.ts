343dc6f4df0d6003de5474f16e29bbcd616c35d2
Loukas Kotas <loukas.kotas@gmail.com>
2024-10-30 20:27:06 +0200

    show message for refreshing collections

--- src/commands/refresh-tree.ts
+++ src/commands/refresh-tree.ts
@@ -1,10 +1,13 @@
 import { CommandType } from "../command-type.model";
 import { WorkspaceState } from "../global-state";
+import { showMessageWithTimeout } from "../helpers/messages";
 
 export function RefreshTreeCommand() {
   const globalState = WorkspaceState.getInstance();
   if (globalState.collectionProvider && globalState.context) {
     globalState.collectionProvider.refresh();
     globalState.lastActionApplied = CommandType.RefreshTree;
+    // Show informational message
+    showMessageWithTimeout(`Refreshing Collections`, 2000);
   }
 }
